66a02fcc3f12b1da1a4f9d06ad4a894abf42f923
moonycode <chl93m@naver.com>
2023-08-04 17:59:58 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,16 @@
+# mysql connect package
+# pip --> conda
+
+# connect Mysql
+# ip, port, username, password, database name
+
+# editor - statement
+
+# make select query
+
+# execute select query
+
+# return result set , display
+
+# close
+
